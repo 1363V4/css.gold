897485717a72d42d1ccbf70777f4ae6192b06f55
1363v4 <louisemmanuelgires@gmail.com>
2024-09-17 17:34:21 +0200

    dev

--- static/js/manifesto.js
+++ static/js/manifesto.js
@@ -1,22 +1,6 @@
 var desktop = $("#desktop");
 var phone = $("#phone");
 
-var links = $$("a");
-var texts = $$("div[id*='text']", desktop);
-links.forEach(function(link) {
-    link.addEventListener("click", function() {
-        links.forEach(function(link) {
-            link.parentNode.classList.remove("selected");
-        });
-        link.parentNode.classList.add("selected");
-        var target = `text_${link.id.split('_')[1]}`;
-        texts.forEach(function(text) {
-            text.classList.add("hidden");
-        });
-        $(`div[id='${target}']`, desktop).classList.remove("hidden");
-    });
-});
-
 var arrow = $("#arrow");
 var phone_text = $("#phone_text");
 arrow.addEventListener("click", function() {
