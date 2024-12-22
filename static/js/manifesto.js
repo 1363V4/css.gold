4d0b98332aeb59d056aa688ce219803b8414edd9
1363v4 <louisemmanuelgires@gmail.com>
2024-12-22 21:02:39 +0100

    dev

--- static/js/manifesto.js
+++ static/js/manifesto.js
@@ -1,17 +1,31 @@
-var links = $$("a");
-links.forEach(function(link) {
-    link.addEventListener("click", function() {
-        links.forEach(function(link) {
-            link.parentNode.classList.remove("selected");
-        });
-        link.parentNode.classList.add("selected");
-    });
-});
+var phoneDisplay = 1;
 
-var arrow = $("#arrow");
-var phone_text = $("#phone_text");
-arrow.addEventListener("click", function() {
-    var currentNum = parseInt(phone_text.getAttribute("data-num")) || 1;
-    var nextNum = (currentNum % 13) + 1;
-    phone_text.setAttribute("data-num", nextNum);
+function updatePhone() {
+    $$("#texts div").forEach(div => {
+        div.style.display = div.id === phoneDisplay.toString() ? "block" : "none";
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    var isDesktop = window.innerWidth > 768;
+    document.body.classList.add(isDesktop ? "gg-01" : "gg-10-v");
+    if (isDesktop) {
+        document.body.classList.add("gg-01");
+        $("#arrow").classList.add("hidden");
+        $$("#links div a").forEach(link => {
+            link.addEventListener("click", function() {
+                $$("#links div").forEach(div => div.classList.remove("selected"));
+                this.parentNode.classList.add("selected");
+            });
+        });
+    } else {
+        document.body.classList.add("gg-10-v");
+        $("#links").classList.add("hidden");
+        $("#arrow").addEventListener("click", function() {
+            phoneDisplay = (phoneDisplay % 13) + 1;
+            console.log(phoneDisplay);
+            updatePhone();
+        });
+        updatePhone();
+    }
 });
